b126cc6eee44da9c26e826bbdf6b9fde48f257af
Gabriel Patara <gabriel.patara@gmail.com>
2024-04-23 15:21:52 -0300

    nome flutuante

--- server/app.js
+++ server/app.js
@@ -7,32 +7,50 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+app.use(express.urlencoded({ extended: true }));
+
+var nome = '';
 var Players = [];
-app.get('/', (req, res) => {
+
+app.get('/game', (req, res) => {
+  res.sendFile(join(__dirname, '../index.html'));
+});
+
+app.get('/',(req,res)=>{
+  res.sendFile(join(__dirname,'../home.html'));
+});
+
+app.post('/', (req, res) => {
+  const input = req.body.nome;
+  nome = input;
+
   res.sendFile(join(__dirname, '../index.html'));
 });
 
 io.on('connection', (socket) => {
-  Players.push({ id: socket.id, x: 0, y: 0 })
-  socket.emit('Players', Players);
-  console.log('Novo usuário conectado. ID do socket:', socket.id);
+  if(nome !== ''){
 
-
-  socket.on('move', (coords) => {
-    Players = Players.map((e)=>{
-      if(e.id == socket.id){
-        e.x = coords.x;
-        e.y = coords.y;
-      }
-      return e;
-    });
+    Players.push({ id: socket.id, x: 0, y: 0, name: nome})
     socket.emit('Players', Players);
-  });
+    console.log('Novo usuário conectado. ID do socket:', socket.id);
 
-  socket.on('disconnect', () => {
-    Players = Players.filter((e)=>e.id !== socket.id);
-    console.log('Usuário desconectado. ID do socket:', socket.id);
-  });
+
+    socket.on('move', (coords) => {
+      Players = Players.map((e)=>{
+        if(e.id == socket.id){
+          e.x = coords.x;
+          e.y = coords.y;
+        }
+        return e;
+      });
+      socket.emit('Players', Players);
+    });
+
+    socket.on('disconnect', () => {
+      Players = Players.filter((e)=>e.id !== socket.id);
+      console.log('Usuário desconectado. ID do socket:', socket.id);
+    });
+  };
 });
 
 server.listen(3000, () => {
